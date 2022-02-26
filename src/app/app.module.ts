b3514f54c2c4568722d705c46e6498252538f626
shivanibg44 <shivanibg5sonu@gmail.com>
2022-02-27 03:51:03 +0530

    committe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './home/nav/nav.component';
 import { FooComponent } from './home/foo/foo.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooComponent
+    FooComponent,
+    LandingComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
