a94eb74447ed207e6a71735ecaae546ab20a9c86
shivanibg44 <shivanibg5sonu@gmail.com>
2022-03-01 17:48:03 +0530

    committ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Infinity'; 
-  public image = "assets/img/pp1.jpg"
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './home/nav/nav.component';
-import { FooComponent } from './home/foo/foo.component';
-import { LandingComponent } from './pages/landing/landing.component';
-import { LoginComponent } from './pages/login/login.component';
-import { SignupComponent } from './pages/signup/signup.component';
-import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
-    FooComponent,
-    LandingComponent,
-    LoginComponent,
-    SignupComponent,
-    AboutComponent
-  
+    
     
   ],
   imports: [
